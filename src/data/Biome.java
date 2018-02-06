57dc4e7ee932f5bd92c96c222caae49793b479f0
tsuku <tsuku@10.77.112.65>
2018-02-06 15:23:29 +0100

    Added data classes getter/setter

---
+++ src/data/Biome.java
@@ -0,0 +1,14 @@
+package data;
+
+public class Biome {
+	
+	private String biomeType;
+	
+	public void setBiomeType( String biomeName) {
+		biomeType = biomeName;
+		
+	}
+	public String getBiomeType() {
+		return biomeType;
+	}
+}
---
+++ src/data/Location.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Location {
+
+	private int absciss;
+	private int ordinate;
+	
+	
+	public void setAbsciss(int x) {
+		absciss = x;
+	}
+
+	public int getAbsciss() {
+		return absciss;
+	}
+	
+	
+	public void setOrdinate(int y) {
+		ordinate = y;
+	}
+
+	public int getOrdinate() {
+		return ordinate;
+	}
+}
+
